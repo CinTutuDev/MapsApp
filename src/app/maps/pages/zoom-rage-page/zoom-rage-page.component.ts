de340aafa241a506815ab8affb3ed1852db8c1ca
cinta <cintatafur@gmail.com>
2024-01-09 12:08:48 +0100

    🌐Coordenadas(zoom,  LngLat, AfterViewInit, OnDestroy):obtener coordenadas en map

--- src/app/maps/pages/zoom-rage-page/zoom-rage-page.component.ts
+++ src/app/maps/pages/zoom-rage-page/zoom-rage-page.component.ts
@@ -1,21 +1,73 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
-import { Map } from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnDestroy,
+  ViewChild,
+} from '@angular/core';
+import { LngLat, Map } from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 @Component({
   templateUrl: './zoom-rage-page.component.html',
   styleUrls: ['./zoom-rage-page.component.scss'],
 })
-export class ZoomRagePageComponent implements AfterViewInit {
+export class ZoomRagePageComponent implements AfterViewInit, OnDestroy {
+  public zoom: number = 10;
+  public map?: Map;
+  public currentLngLat: LngLat = new LngLat(
+    -4.461836858435845,
+    40.31791851561749
+  );
+
   @ViewChild('map') divMap?: ElementRef;
 
   ngAfterViewInit(): void {
     if (!this.divMap) throw 'El elemento HTML no fue encontrado';
 
     console.log(this.divMap);
-    const map = new Map({
+    this.map = new Map({
       container: this.divMap.nativeElement, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
-      center: [-74.5, 40], // starting position [lng, lat]
-      zoom: 9, // starting zoom
+      center: this.currentLngLat, // starting position [lng, lat]
+      zoom: this.zoom, // starting zoom
     });
+
+    this.mapListener();
+  }
+
+  ngOnDestroy(): void {
+    this.map?.remove();
+  }
+
+  mapListener() {
+    if (!this.map) throw 'Mapa no inicializado';
+
+    this.map.on('zoom', (ev) => {
+      this.zoom = this.map!.getZoom();
+    });
+
+    this.map.on('zoomend', (ev) => {
+      if (this.map!.getZoom() < 18) return;
+
+      this.map!.zoomTo(18);
+    });
+
+    this.map.on('move', () => {
+      this.currentLngLat = this.map!.getCenter();
+      /* const { lng, lat } = this.currentLngLat; */
+      /*  console.log({lng, lat}); */
+    });
+  }
+
+  zoomIn() {
+    this.map?.zoomIn();
+  }
+
+  zoomOut() {
+    this.map?.zoomOut();
+  }
+
+  zoomChanged(value: string) {
+    this.zoom = Number(value);
+    this.map?.zoomTo(this.zoom);
   }
 }
